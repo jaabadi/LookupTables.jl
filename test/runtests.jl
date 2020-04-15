211cfb2ddd5f6cce423226b2304853577117606f
Joseph Abadi <jaabadi@princeton.edu>
2020-04-14 22:31:16 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,26 @@ using LookupTables
 using Test
 
 @testset "LookupTables.jl" begin
-    # Write your own tests here.
+    nx = 100
+    ny = 50
+
+    y = range(13.7, step = 0.5, length = 50)
+    x = range(0.0, step = 0.1, length = 100)
+
+    vals = zeros(nx, ny)
+
+    for i in eachindex(x)
+        for j in eachindex(y)
+            vals[i, j] = x[i]^2 - 3 * x[i] * y[j]
+        end
+    end
+
+    ltable = LookupTable2D(x, y, vals)
+
+    using BenchmarkTools
+
+    @btime lookup(ltable, 4.4, 22.2)
+
+    @btime [[lookup(ltable, xval, yval) for xval in x] for yval in y]
+
 end
